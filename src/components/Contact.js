38066b1175c8bfbf87654c18c7b07b64519c39a3
sconsultancy <148803608+sconsultancy@users.noreply.github.com>
2024-06-09 07:13:04 +0530

    corrected 3

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -55,7 +55,7 @@ function Contact() {
               </Link>
             </div>
             <img
-              src="/assets/contact_image.png"
+              src="assets/contact_image.png"
               alt=""
               className="footer_image  max-[440px]:w-4/5"
             />
